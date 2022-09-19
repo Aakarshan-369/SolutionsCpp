102407a2a7187ae5f33b20efc398f394749bd0be
Aakarshan-369 <akarshan.mit@gmail.com>
2022-09-19 21:54:52 +0530

    Updated Variable name

--- sortedcabs.cpp
+++ sortedcabs.cpp
@@ -5,21 +5,21 @@ using namespace std;
 vector<pair<int,int>> sortCabs(vector<pair<int,int>> v){
    
     double DistanceFromOrigin ;
-    vector <pair < double  ,pair <int,int>> > workervec ;
-    workervec.resize(v.size()) ;
+    vector <pair < double  ,pair <int,int>> > WorkerVectorStoringPairWithDistance ;
+    WorkerVectorStoringPairWithDistance.resize(v.size()) ;
     vector <pair <int,int> > FinalSorted ; int i =0 ;
     FinalSorted.resize(v.size());
      for(auto x: v) {
-         workervec[i].first = DistanceFromOrigin ; 
+         WorkerVectorStoringPairWithDistance[i].first = DistanceFromOrigin ; 
        DistanceFromOrigin = (sqrt(x.first*x.first + x.second*x.second)) ;
-        workervec[i].second.first = x.first;
-        workervec[i].second.second = x.second ;
+        WorkerVectorStoringPairWithDistance[i].second.first = x.first;
+        WorkerVectorStoringPairWithDistance[i].second.second = x.second ;
         i++;
         
     }
     i =0 ; 
-    sort (workervec.begin(), workervec.end());
-    for(auto x : workervec) {
+    sort (WorkerVectorStoringPairWithDistance.begin(), WorkerVectorStoringPairWithDistance.end());
+    for(auto x : WorkerVectorStoringPairWithDistance) {
         FinalSorted[i].first = x.second.first ;
         FinalSorted[i].second = x.second.second ;
         i++ ;
